0a759583b7c12cd95e9b695c0e513cf15597300d
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-03 18:33:06 +0530

    Updated Pytest module to test pulling metaweather data from api and upload to s3

--- api_pull_metaweather_data/test_pull_api_metaweather_data_s3.py
+++ api_pull_metaweather_data/test_pull_api_metaweather_data_s3.py
@@ -4,8 +4,11 @@ import os
 import logging
 import configparser
 from datetime import datetime
+from time import time
 import pytest
-from metaweather_details_api_partition_s3 import PullMetaWeatherDataUploadS3
+import pandas as pd
+import requests
+from metaweather_data_api_partition_s3 import PullMetaWeatherDataUploadS3
 from metaweather_info_partition_local import MetaweatherPartitionLocal
 from pull_data_each_city_from_api import PullDataFromMetaweatherApi
 from aws_s3.s3_details import S3Details
@@ -23,20 +26,42 @@ logging.basicConfig(
     filemode="w",
 )
 logger_obj = logging.getLogger()
-start_date = "2022-04-27"
-end_date = "2022-04-29"
+
+
+@pytest.fixture
+def start_date():
+    """This method returns the start_date"""
+    return "2022-05-02"
+
+
+@pytest.fixture
+def end_date():
+    """This method returns the end_date"""
+    return "2022-05-03"
+
+
+@pytest.fixture
+def city():
+    """This method returns the cityname"""
+    return "albuquerque"
+
+
+@pytest.fixture
+def woeid_id():
+    """This method returns the woeid of city"""
+    return "2352824"
 
 
 class TestMetaWeatherApi:
-    """This Test Class will check for all the possible Test cases in 
-        metaweather details api partion s3 module"""
+    """This Test Class will check for all the possible Test cases in
+    metaweather details api partion s3 module"""
 
-    def test_pull_metaweather_api_objects(self):
+    def test_pull_metaweather_api_objects(self, start_date, end_date):
         """This tests for the instance belong to the class PullMetaWeatherDataUploads3"""
         self.obj_metaapi = PullMetaWeatherDataUploadS3(logger_obj, start_date, end_date)
         assert isinstance(self.obj_metaapi, PullMetaWeatherDataUploadS3)
 
-    def test_metaweather_data_for_givendates_passed(self):
+    def test_metaweather_data_for_givendates_passed(self, start_date, end_date):
         """This methods tests the days between the given dates"""
         startdate = datetime.strptime(start_date, "%Y-%m-%d").date()
         enddate = datetime.strptime(end_date, "%Y-%m-%d").date()
@@ -46,7 +71,7 @@ class TestMetaWeatherApi:
         assert not isinstance(date, datetime)
 
     @pytest.mark.xfail
-    def test_metaweather_data_for_givendates_failed(self):
+    def test_metaweather_data_for_givendates_failed(self, start_date, end_date):
         """This methods tests failed case for days between the given dates"""
         startdate = datetime.strptime(start_date, "%Y-%m-%d").date()
         enddate = datetime.strptime(end_date, "%Y-%m-%d").date()
@@ -55,7 +80,46 @@ class TestMetaWeatherApi:
         date = self.obj_metaapi.get_metaweather_data_for_givendates(startdate, enddate)
         assert isinstance(date, datetime)
 
-    def test_exceution_last_date_isupdated(self):
+    def test_filter_group_response_by_hour_isdone(self, start_date, end_date, city, woeid_id):
+        """This method tests for filtering the response from api based on hours"""
+        search_date = datetime.now().date().strftime("%Y/%m/%d")
+        response = requests.get(
+            "https://www.metaweather.com/api/location/{woeid}/{date}/".format(
+                woeid=woeid_id, date=search_date
+            )
+        ).json()
+        self.obj_metaapi = PullMetaWeatherDataUploadS3(logger_obj, start_date, end_date)
+        result = self.obj_metaapi.filter_group_response_by_hour(response, city, search_date)
+        assert result is True
+
+    @pytest.mark.xfail
+    def test_filter_group_response_by_hour_is_notdone(self, start_date, end_date, city, woeid_id):
+        """This method tests failure case for filtering the response from api based on hours"""
+        search_date = datetime.now().date().strftime("%Y/%m/%d")
+        response = requests.get(
+            "https://www.metaweather.com/api/location/{woeid}/{date}/".format(
+                woeid=woeid_id, date=search_date
+            )
+        ).json()
+        self.obj_metaapi = PullMetaWeatherDataUploadS3(logger_obj, start_date, end_date)
+        result = self.obj_metaapi.filter_group_response_by_hour(response, city, search_date)
+        assert result is False
+
+    def test_put_partition_path_isdone(self, start_date, end_date, city):
+        """This method tests for giving the partition path based on city,year,month,date.hour"""
+        self.obj_metaapi = PullMetaWeatherDataUploadS3(logger_obj, start_date, end_date)
+        response = self.obj_metaapi.put_partition_path(city, "2022-05-03T07")
+        assert response == "pt_city=albuquerque/pt_year=2022/pt_month=05/pt_day=03/pt_hour=07/"
+
+    @pytest.mark.xfail
+    def test_put_partition_path_is_notdone(self, start_date, end_date, city):
+        """This method tests failure case for giving the partition path
+        based on city,year,month,date.hour"""
+        self.obj_metaapi = PullMetaWeatherDataUploadS3(logger_obj, start_date, end_date)
+        response = self.obj_metaapi.put_partition_path(city, "2022-05-03T07")
+        assert response != "pt_city=albuquerque/pt_year=2022/pt_month=05/pt_day=03/pt_hour=07/"
+
+    def test_exceution_last_date_isupdated(self, start_date, end_date):
         """This method tests for the last date of execution is updated in config file"""
         self.obj_metaapi = PullMetaWeatherDataUploadS3(logger_obj, start_date, end_date)
         last_date = self.obj_metaapi.last_date_of_execution("")
@@ -64,7 +128,7 @@ class TestMetaWeatherApi:
         assert last_date == datetime.now().date()
 
     @pytest.mark.xfail
-    def test_exceution_last_date_is_notupdated(self):
+    def test_exceution_last_date_is_notupdated(self, start_date, end_date):
         """This method tests for the last date of execution is updated in config file"""
         self.obj_metaapi = PullMetaWeatherDataUploadS3(logger_obj, start_date, end_date)
         self.obj_metaapi.last_date_of_execution()
@@ -80,35 +144,33 @@ class TestPullDataForEachCity:
         self.obj_pullapi = PullDataFromMetaweatherApi(logger_obj)
         assert isinstance(self.obj_pullapi, PullDataFromMetaweatherApi)
 
-    def test_pull_data_from_each_city_passed(self):
-        """ This method tests for pulling metaweather data from each city 
-            with their woeid and date"""
+    def test_pull_data_from_each_city_passed(self, start_date, woeid_id):
+        """This method tests for pulling metaweather data from each city
+        with their woeid and date"""
         self.obj_pullapi = PullDataFromMetaweatherApi(logger_obj)
-        woeid = "2352824"
         startdate = datetime.strptime(start_date, "%Y-%m-%d").date()
         search_date = startdate.strftime("%Y/%m/%d")
         api_response = self.obj_pullapi.get_weather_data_cities_using_woeid_from_api(
-            woeid, search_date
+            woeid_id, search_date
         )
         assert api_response is not None
 
     @pytest.mark.xfail
-    def test_pull_data_from_each_city_failed(self):
+    def test_pull_data_from_each_city_failed(self, start_date, woeid_id):
         """This method tests failure case for pulling metaweather data from
-           each city with their woeid and date"""
+        each city with their woeid and date"""
         self.obj_pullapi = PullDataFromMetaweatherApi(logger_obj)
-        woeid = "2352824"
         startdate = datetime.strptime(start_date, "%Y-%m-%d").date()
         search_date = startdate.strftime("%Y/%m/%d")
         api_response = self.obj_pullapi.get_weather_data_cities_using_woeid_from_api(
-            woeid, search_date
+            woeid_id, search_date
         )
         assert api_response is None
 
 
 class TestS3Details:
     """This Test Class will check for all the possible
-        Test cases in S3Operations"""
+    Test cases in S3Operations"""
 
     def test_s3operations_objects(self):
         """This Method will test for the instance belong to the class S3Operations"""
@@ -126,31 +188,35 @@ class TestS3Details:
         """This method will test for the report is uploaded to S3 in class S3Operations"""
         self.s3_client = S3Details(logger_obj)
         res = self.s3_client.upload_file(b"flow of code", "flow.txt")
-        assert res != "flow of code"
+        assert res == "flow of"
 
 
 class TestUploadPartitionLocal:
     """This class has methods to test possible testcases in
-        metaweather parition upload local module"""
+    metaweather parition upload local module"""
+
     def test_parition_local_object(self):
         """This method tests the instance of class MetaweatherPartitionlocal"""
         self.obj_local = MetaweatherPartitionLocal(logger_obj)
-        assert isinstance(self.obj_local,MetaweatherPartitionLocal)
+        assert isinstance(self.obj_local, MetaweatherPartitionLocal)
 
-    def test_write_data_to_local_file_is_done(self):
-        """ This method will test for the data is written to local file 
-            in class Metaweather Partition local"""
+    def test_write_data_to_local_file_is_done(self, city):
+        """This method will test for the data is written to local file
+        in class Metaweather Partition local"""
         self.obj_local = MetaweatherPartitionLocal(logger_obj)
-        path = "local/test/metaweather_api/"
-        local_file = self.obj_local.upload_parition_s3_local(path, "weather_data")
+        file_name = f"metaweather_{city}_{int(time())}.json"
+        partition_path = "pt_city=albuquerque/pt_year=2022/pt_month=05/pt_day=03/pt_hour=07/"
+        new_df = pd.DataFrame()
+        local_file = self.obj_local.upload_parition_s3_local(new_df, file_name, partition_path)
         assert local_file == "json file created"
-
 
     @pytest.mark.xfail
     def test_write_data_to_local_file_is_not_done(self):
-        """ This method will test for the data is not written
-            to local file in class Metaweather PartitionLocal """
+        """This method will test for the data is not written
+        to local file in class Metaweather PartitionLocal"""
         self.obj_local = MetaweatherPartitionLocal(logger_obj)
-        path='local/test/metaweather_api/'
-        local_file = self.obj_local.upload_parition_s3_local(path, 'weather_data')
-        assert local_file=='json file not created'
+        file_name = f"metaweather_{city}_{int(time())}.json"
+        partition_path = "pt_city=albuquerque/pt_year=2022/pt_month=05/pt_day=03/pt_hour=07/"
+        new_df = pd.DataFrame()
+        local_file = self.obj_local.upload_parition_s3_local(new_df, file_name, partition_path)
+        assert local_file == "json file not created"
