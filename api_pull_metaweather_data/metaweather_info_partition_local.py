81f096499267320d5ce6ece0e527e2419bfd85b6
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-06 13:53:03 +0530

    module contains script for uploading data in json format in partition path in local

--- api_pull_metaweather_data/metaweather_info_partition_local.py
+++ api_pull_metaweather_data/metaweather_info_partition_local.py
@@ -1,6 +1,7 @@
 """This module contains script for uploading data in json format in partiton path"""
 import os
 import configparser
+import shutil
 
 parent_dir = os.path.dirname(os.getcwd())
 config = configparser.ConfigParser()
@@ -12,22 +13,21 @@ class MetaweatherPartitionLocal:
 
     def __init__(self, logger):
         self.logger = logger
-        self.local_apipath = os.path.join(
-            parent_dir, config["metaweather_api"]["local_file_path"]
-        )
+        self.local_apipath = os.path.join(parent_dir, config["metaweather_api"]["local_file_path"])
 
-    def upload_parition_s3_local(self, new_df, file_name, partition_path):
+    def upload_parition_s3_local(self, copy_source, file_name, partition_path):
         """This method uploads weatherdata in the partition path in the form of json"""
         try:
             new_dir = self.local_apipath + "/" + partition_path + "/"
             if not os.path.exists(new_dir):
                 os.makedirs(new_dir)
-            new_df.to_json(new_dir + file_name, orient="records", lines=True)
+            # new_df.to_json(new_dir + file_name, orient="records", lines=True)
+            shutil.copy(copy_source, new_dir)
             print("Successfully created json file in the given path\n")
             self.logger.info("Successfully created json file in the given path")
 
         except Exception as err:
             print("Cannot upload the json file in the given path:", err)
             self.logger.error("Cannot upload the json file in the given path")
-            file_name= None
+            file_name = None
         return file_name
