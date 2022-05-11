5d6264af35e129f098ad8d0c1447889198ecb476
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-11 18:30:16 +0530

    Updated module with limited from dataframes to get employees details by date of joining and upload to s3 in that partition of joining date

--- get_employee_details_sql_date_joining/employee_details_sql_partition_s3.py
+++ get_employee_details_sql_date_joining/employee_details_sql_partition_s3.py
@@ -11,8 +11,7 @@ import sys
 from time import time
 from get_employee_details_sql import GetEmployeeFromSql
 from employee_details_upload_local import EmployeeDetailsPartitionLocalUpload
-
-from aws_s3.s3_details import S3Details
+# from aws_s3.s3_details import S3Details
 
 parent_dir = os.path.dirname(os.getcwd())
 config = configparser.ConfigParser()
@@ -35,53 +34,42 @@ class EmployeeDetailsJoiningDate:
             os.makedirs(self.local_sqlpath)
         self.local = EmployeeDetailsPartitionLocalUpload(logger_obj)
         self.sql = GetEmployeeFromSql(logger_obj)
-        self.s3_client = S3Details(logger_obj)
+        # self.s3_client = S3Details(logger_obj)
 
     def get_employee_details_for_givendates(self):
         """This method is to get the employee details for the given dates"""
-        if self.enddate <= (datetime.now().date()):
+        if self.startdate <= (datetime.now().date()-timedelta(days=1)):
             if self.startdate < self.enddate:
                 dates = {"date1": self.startdate, "date2": self.enddate}
             else:
                 self.logger.info("Cannot fetch details as startdate is greater than enddate")
-                print("Cannot fetch the details between", self.startdate, "and", self.enddate)
                 sys.exit("script was terminated since startdate is greater than enddate")
             self.logger.info("Filtering Employee details from given startdate to enddate")
             print("Filtering Employee details from given startdate to enddate")
         else:
             self.logger.info("Cannot fetch the details for future dates")
-            print("Cannot fetch the details for future dates")
             sys.exit("script was terminated when fetching for future dates")
         return dates
 
     def employee_details_from_sql(self):
         """This method gets the employee details from sql and filter based on dates"""
         dates = self.get_employee_details_for_givendates()
-        response = self.sql.get_query_from_where_condition(
+        response = self.sql.get_datas_from_sql(
             dates["date1"], self.condition, dates["date2"]
         )
-        print(response)
+        # print(response)
         if response is not None:
             day_count = (dates["date2"] - dates["date1"]).days + 1
             for day in range(day_count):
                 single_date = dates["date1"] + timedelta(day)
-                # print(single_date)
-                self.limit_dataframe_from_response(response, single_date)
+                self.logger.info("Filtering the response based on dates")
+                self.filter_create_response_by_date(response, single_date)
         else:
-            print("Cannot fetch the data from sql due to problem in sql ")
             self.logger.error("Cannot fetch the data from sql due to problem in sql")
+            single_date=None
             sys.exit("The script has terminated due to problem in sql")
-
-    def limit_dataframe_from_response(self, response, single_date):
-        """This method limits the dataframe with row limit and filter them"""
-        index = int(config["sql_employee_details_joiningdate"]["index"])
-        limit = int(config["sql_employee_details_joiningdate"]["row_limit"])
-        while index < len(response):
-            limit_df = response.iloc[index : index + limit]
-            self.filter_create_response_by_date(limit_df, single_date)
-            self.logger.info("Filtering the response based on dates")
-            index = index + limit
-
+        return single_date
+    
     def filter_create_response_by_date(self, df_data, date):
         """This method filters and creates a newdataframe from the response
         based on date using pandas"""
@@ -97,15 +85,15 @@ class EmployeeDetailsJoiningDate:
                 )
                 partition_path = self.put_partition_path(date)
                 copy_source = self.local_sqlpath + "/" + file_name
-                self.local.upload_parition_s3_local(
+                file_name=self.local.upload_parition_s3_local(
                     self.local_sqlpath, copy_source, file_name, partition_path
                 )
                 # self.upload_to_s3(copy_source,partition_path, file_name)
-            return True
         except Exception as err:
             print("Cannot filter the datas in dataframe due to this error:", err)
             self.logger.error("Cannot filter the datas in dataframe due to an error")
-            return False
+            file_name=None
+        return file_name
 
     def put_partition_path(self, date):
         """This method will make partion path based on year,month,date
@@ -130,11 +118,12 @@ class EmployeeDetailsJoiningDate:
 def valid_date(date):
     """This method checks for the valid date entered and returns in datetime.date() format"""
     try:
-        return datetime.strptime(date, "%Y-%m-%d").date()
+        date= datetime.strptime(date, "%Y-%m-%d").date()
     except ValueError:
+        date=None
         msg = "not a valid date: {0!r}".format(date)
         raise argparse.ArgumentTypeError(msg)
-
+    return date
 
 def main():
     """This is the main method for this module"""
@@ -167,6 +156,7 @@ def main():
     )
     parser.add_argument(
         "--condition",
+        type=str,
         help="Condition to use in sql query",
         default=config["sql_employee_details_joiningdate"]["where_condition"],
     )
