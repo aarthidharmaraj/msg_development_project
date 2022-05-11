2cda04fc5e440936c59236d70ab03831c7105357
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-11 18:28:29 +0530

    Updated Module to get the employee details from sql for given dates with generalized query with condition

--- get_employee_details_sql_date_joining/get_employee_details_sql.py
+++ get_employee_details_sql_date_joining/get_employee_details_sql.py
@@ -4,6 +4,7 @@ import configparser
 from tkinter import YES
 import pyodbc
 import pandas as pd
+from sqlalchemy.engine import URL, create_engine
 
 parent_dir = os.path.dirname(os.getcwd())
 config = configparser.ConfigParser()
@@ -16,25 +17,25 @@ class GetEmployeeFromSql:
     def __init__(self, logger):
         """This is the init method for the class"""
         self.logger = logger
-        self.driver = config["sql_server"]["driver"]
-        self.host = config["sql_server"]["host"]
-        self.database = config["sql_server"]["database"]
-        self.table = config["sql_server"]["table"]
-        self.user = config["sql_server"]["user"]
-        self.password = config["sql_server"]["password"]
 
-    def sql_connection(self):
+    def sql_connection(self, driver, host, database, user, password):
         """This method is to connect the sql server using pyodbc"""
+        # for drivers in pyodbc.drivers():
+        #     print(drivers)
         try:
             conn = pyodbc.connect(
-                Driver=self.driver,
-                host=self.host,
-                database=self.database,
-                user=self.user,
-                password=self.password,
+                Driver=driver,
+                host=host,
+                database=database,
+                user=user,
+                password=password,
                 trusted_connection=YES,
                 autocommit=True,
             )
+            # connection_string="DRIVER={ODBC Driver 18 for SQL Server};host=localhost;DATABASE=Employee_payroll;UID=root;PWD=Aspire@123"
+            # connection_string="Driver={driver};Server='{host}';Database='{database}';User='{user}';Password='{password}';trusted_connection='yes';auto_commit='True'"
+            # connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
+            # conn = create_engine(connection_url)
             self.logger.info("The connection has been established successfuly")
         except Exception as err:
             self.logger.info("Cannot connect to server")
@@ -42,38 +43,56 @@ class GetEmployeeFromSql:
             conn = None
         return conn
 
-    def get_employee_details_by_joiningdate(self, query):
+    def get_employee_details_by_joiningdate(self, driver, host, database, user, password, query):
         """This method is to get the employee details from sql by filtering"""
         try:
-            conn = self.sql_connection()
-            # query = "SELECT * from employee where Date_of_Joining between ?
-            # and ? Order By Date_of_Joining asc"
-            # df_data=pd.read_sql(query, conn, params=(startdate, enddate))
-            df_data = pd.read_sql(query, conn)
+            conn = self.sql_connection(driver, host, database, user, password)
+            # chunk_data=[]
+            # for df_data in pd.read_sql(query, conn,chunksize=2):
+            #     chunk_data.append(df_data)
+            # data=pd.concat(chunk_data)
+            chunk_data = pd.read_sql(query, conn, chunksize=1000)
+            df_data = pd.concat(chunk_data)
         except Exception as err:
             print("Cannot filter the employee details for given date", err)
             self.logger.error("Cannot filter the employee details for given date")
             df_data = None
         return df_data
 
-    def get_query_from_where_condition(self, start, condition, end):
+    def get_query_from_where_condition(self, start, condition, end, table, column):
         """This method gets the query from where condition"""
-        table = config["sql_server"]["table"]
-        column = config["sql_server"]["column"]
         # end=None
         try:
             if end is not None and condition.lower() == "between":
                 query = f"SELECT * from {table} where {column} {condition} '{start}'and '{end}' Order By {column} asc"
-                response = self.get_employee_details_by_joiningdate(query)
             elif end is None and condition.lower() != "between":
                 query = f"SELECT * FROM {table}  WHERE {column} {condition} '{start}' Order By {column} asc"
-                response = self.get_employee_details_by_joiningdate(query)
             else:
                 print("Cannot get the data for the given condition", condition)
                 self.logger.info("Cannot get the data for the given condition")
-                response = None
+                query = None
         except Exception as err:
             print("Cannot get the query", err)
             self.logger.info("Cannot execute the query")
+            query = None
+        return query
+
+    def get_datas_from_sql(self, start, condition, end):
+        """This method gets required datas from config for connection"""
+        driver = config["sql_server"]["driver"]
+        host = config["sql_server"]["host"]
+        database = config["sql_server"]["database"]
+        user = config["sql_server"]["user"]
+        password = config["sql_server"]["password"]
+        table = config["sql_server"]["table"]
+        column = config["sql_server"]["column"]
+        try:
+            get_query = self.get_query_from_where_condition(start, condition, end, table, column)
+            print(get_query)
+            response = self.get_employee_details_by_joiningdate(
+                driver, host, database, user, password, get_query
+            )
+        except Exception:
+            self.logger.info("Cannot get the response for given dates")
             response = None
         return response
