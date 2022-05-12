7275901fac5ead71fd365138fe06290b1abef4e6
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-12 18:39:23 +0530

    updated with rectified sqlalchemy warning// pending

--- get_employee_details_sql_date_joining/get_employee_details_sql.py
+++ get_employee_details_sql_date_joining/get_employee_details_sql.py
@@ -4,6 +4,7 @@ import configparser
 from tkinter import YES
 import pyodbc
 import pandas as pd
+import sys
 from sqlalchemy.engine import URL, create_engine
 
 parent_dir = os.path.dirname(os.getcwd())
@@ -11,7 +12,7 @@ config = configparser.ConfigParser()
 config.read(parent_dir + "/details.ini")
 
 
-class GetEmployeeFromSql:
+class EmployeeFromSql:
     """This calss Gets the details of employee from sql"""
 
     def __init__(self, logger):
@@ -20,8 +21,8 @@ class GetEmployeeFromSql:
 
     def sql_connection(self, driver, host, database, user, password):
         """This method is to connect the sql server using pyodbc"""
-        # for drivers in pyodbc.drivers():
-        #     print(drivers)
+        for drivers in pyodbc.drivers():
+            print(drivers)
         try:
             conn = pyodbc.connect(
                 Driver=driver,
@@ -32,10 +33,10 @@ class GetEmployeeFromSql:
                 trusted_connection=YES,
                 autocommit=True,
             )
-            # connection_string="DRIVER={ODBC Driver 18 for SQL Server};host=localhost;DATABASE=Employee_payroll;UID=root;PWD=Aspire@123"
-            # connection_string="Driver={driver};Server='{host}';Database='{database}';User='{user}';Password='{password}';trusted_connection='yes';auto_commit='True'"
-            # connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-            # conn = create_engine(connection_url)
+            # connection_string=config["sql_server"]["connection_string"]
+            # print(connection_string)
+            # connection_url=URL.create("mssql+pyodbc",query={"odbc_connect":connection_string})
+            # conn=create_engine(connection_url)
             self.logger.info("The connection has been established successfuly")
         except Exception as err:
             self.logger.info("Cannot connect to server")
@@ -47,12 +48,9 @@ class GetEmployeeFromSql:
         """This method is to get the employee details from sql by filtering"""
         try:
             conn = self.sql_connection(driver, host, database, user, password)
-            # chunk_data=[]
-            # for df_data in pd.read_sql(query, conn,chunksize=2):
-            #     chunk_data.append(df_data)
-            # data=pd.concat(chunk_data)
             chunk_data = pd.read_sql(query, conn, chunksize=1000)
             df_data = pd.concat(chunk_data)
+            print(df_data)
         except Exception as err:
             print("Cannot filter the employee details for given date", err)
             self.logger.error("Cannot filter the employee details for given date")
@@ -61,16 +59,15 @@ class GetEmployeeFromSql:
 
     def get_query_from_where_condition(self, start, condition, end, table, column):
         """This method gets the query from where condition"""
-        # end=None
         try:
             if end is not None and condition.lower() == "between":
                 query = f"SELECT * from {table} where {column} {condition} '{start}'and '{end}' Order By {column} asc"
-            elif end is None and condition.lower() != "between":
+            elif end is None and condition in ["<","<=",">=","=",">"]:
                 query = f"SELECT * FROM {table}  WHERE {column} {condition} '{start}' Order By {column} asc"
             else:
-                print("Cannot get the data for the given condition", condition)
                 self.logger.info("Cannot get the data for the given condition")
                 query = None
+                sys.exit("system terminated to get data fails for given condition")
         except Exception as err:
             print("Cannot get the query", err)
             self.logger.info("Cannot execute the query")
