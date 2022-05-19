da976ded62d76f9e552b71c306c4b29f4241f618
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-19 19:18:48 +0530

    Module to get details from local sftp and move files to local s3

---
+++ load_hostaspire_file_sftp_to_s3/sftp_to_s3_upload_local.py
@@ -0,0 +1,56 @@
+"""This module moves the files from sftp to s3 in local"""
+import os
+import shutil
+
+class MoveSftpToS3Local:
+    """This class has the methods that do in local folder for sftp and s3"""
+
+    def __init__(self, logger,config):
+        self.logger = logger
+        self.s3path_source = config["move_asp_from_sftp_to_s3"]["s3_path_source"]
+        self.s3path_stage = config["move_asp_from_sftp_to_s3"]["s3_path_stage"]
+        self.sftp_path=config["move_asp_from_sftp_to_s3"]["sftp_path"]
+
+
+    def list_files_sftp_local(self):
+        """This method gets the list of files names for the given local sftp path by filtering"""
+        try:
+            sftp_file_list = [
+                files for files in os.listdir(self.sftp_path)
+            ]
+        except Exception as err:
+            sftp_file_list=None
+            self.logger.error(f"Cannot get the files from sftp local")
+            print("Cannot get the files from local sftp",err)
+        return sftp_file_list
+    
+    def upload_parition_s3_local(self, local_path, copy_source, file_name, partition_path):
+        """This method uploads weatherdata in the partition path in the form of json"""
+        try:
+            new_dir = local_path + "/" + partition_path + "/"
+            if not os.path.exists(new_dir):
+                os.makedirs(new_dir)
+            shutil.copy(copy_source, new_dir)
+            # os.remove(copy_source)
+            print(
+                f"Successfully created json file'{file_name}' in the given path'{partition_path}'\n"
+            )
+            self.logger.info(
+                f"Successfully created json file {file_name} in the given path {partition_path}"
+            )
+
+        except Exception as err:
+            print("Cannot upload the json file in the given path:", err)
+            self.logger.error(f"Cannot upload the json file in the given path{err}")
+            file_name = None
+        return file_name
+
+    # def rename_file_sftp_local(self, file):
+    #     """This method is used for rename the sftp file or directory after processed"""
+    #     new_name = self.sftp_path + "prcssd." + file
+    #     os.rename(self.sftp_path + file, new_name)
+    #     print("The file", file, "in sftp has been processed and renamed successfully")
+    #     self.logger.info("The file in sftp has been processed and renamed successfully")
+    #     # return 'success'
+    #     return new_name
+
