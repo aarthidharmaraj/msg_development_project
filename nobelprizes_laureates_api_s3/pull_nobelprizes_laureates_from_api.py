c91fd7b6feadbd0b6a5f1ed766ffe1e0de77c296
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-20 12:04:51 +0530

    Updated module to pull response from api

--- nobelprizes_laureates_api_s3/pull_nobelprizes_laureates_from_api.py
+++ nobelprizes_laureates_api_s3/pull_nobelprizes_laureates_from_api.py
@@ -6,16 +6,11 @@ import configparser
 from datetime import date
 import requests
 
-parent_dir = os.path.dirname(os.getcwd())
-config = configparser.ConfigParser()
-config.read(parent_dir + "/details.ini")
-
-
 class NobelprizeLaureatesFromApi:
     """This class has methods to get response for Nobel prizes and laureates
     from the api for the given year"""
 
-    def __init__(self, logger):
+    def __init__(self, logger,config):
         """This is the init method for the class NobelprizeLaureatesFromApi"""
         self.logger = logger
         self.base_url = config["nobelprize_laureates_api"]["basic_url"]
@@ -30,23 +25,23 @@ class NobelprizeLaureatesFromApi:
                 request_url = self.base_url + endpoint
                 response = requests.get(request_url)
                 if response.status_code == 200:
-                    response = response.json()
+                    responses = response.json()
                     self.logger.info(
-                        "Got the response from api for given year with status {response.status_code}"
+                        f"Got the response from api for given year with status{response.status_code}"
                     )
                 else:
                     print("It gives a failure response with code", response.status_code)
                     self.logger.error(
                         f"It gives a failure response with code {response.status_code}"
                     )
-                    response = None
+                    responses = None
             else:
                 self.logger.error(f"Cannot fetch for the given year{year}")
                 print("No responses are available for given year", year)
-                response = None
+                responses = None
 
         except Exception as err:
             self.logger.error(f"Cannot get the response from api due to problem in api{err}")
             print("No response from api", err)
-            response = None
-        return response
+            responses = None
+        return responses
