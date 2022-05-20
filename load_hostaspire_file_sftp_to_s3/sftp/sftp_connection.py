ff9865f211fbe7296f0fd449d1e3e00db63abddf
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-20 18:06:37 +0530

    Sftp module having sftp connection and list files from sftp

--- load_hostaspire_file_sftp_to_s3/sftp/sftp_connection.py
+++ load_hostaspire_file_sftp_to_s3/sftp/sftp_connection.py
@@ -22,24 +22,12 @@ class SftpConnection:
         """This method gets the list of files names for the given sftp path by filtering"""
         try:
             sftp_file_list = [
-                file for file in self.conn.listdir(self.sftp_path) if not file.startswith("prcssd.")
+                file for file in self.conn.listdir(self.sftp_path)
             ]
+            self.logger.info("Received the files from sftp server")
         except Exception as err:
             self.logger.error(f"Cannot able to get the files from sftp server{err}")
             print("Cannot able to get the files from sftp server",err)
             sftp_file_list=None
         return sftp_file_list
 
-    def get_file(self, file, partition_path):
-        """This method get file from sftp without downloading to local file"""
-        try:
-            with self.conn.open(self.sftp_path + file, "r") as file_name:
-                file_name.prefetch()
-                self.logger.info(
-                    "The file has been fetched from sftp and passed upload method in s3"
-                )
-                self.s3_client.upload_file(file_name, partition_path)
-
-        except IOError as ier:
-            print("The file cannot be opened in sftp", ier)
-            self.logger.error("The file cannot be opened in sftp")
