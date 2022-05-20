8788c0ec18668046dbf9d6e4462eec283d7652a3
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-20 12:07:30 +0530

    Updated with logger,endpoints,filename,path changes

--- nobelprizes_laureates_api_s3/nobelprize_laureates_partition_s3.py
+++ nobelprizes_laureates_api_s3/nobelprize_laureates_partition_s3.py
@@ -1,14 +1,12 @@
 """ This module fetches the nobel prizes and laureates based on given year 
 from api,partition them based on year and upload to s3 in that partition_path"""
 
-import logging
-import configparser
 import os
 import argparse
 from datetime import date
 import sys
-from logging.handlers import RotatingFileHandler
 import pandas as pd
+from logger_object_path import LoggerPath
 from nobelprizes_laureates_upload_local import (
     NobelLaureateLocalUpload,
 )
@@ -17,9 +15,7 @@ from pull_nobelprizes_laureates_from_api import (
 )
 from aws_s3.s3_details import S3Details
 
-parent_dir = os.path.dirname(os.getcwd())
-config = configparser.ConfigParser()
-config.read(parent_dir + "/details.ini")
+datas = LoggerPath.logger_object()
 
 
 class NobelPrizeLaureatesPartitionS3:
@@ -29,32 +25,32 @@ class NobelPrizeLaureatesPartitionS3:
 
     def __init__(
         self,
-        logger_obj,
         startyear,
         endyear,
         endpoint,
     ):
         """This is the init method for the class NobelPrizeLaureatesPartitionS3"""
-        self.logger = logger_obj
+        datas = LoggerPath.logger_object()
+        self.logger = datas["logger"]
         self.startyear = startyear
         self.endyear = endyear
         self.endpoint = endpoint
         self.local_path = os.path.join(
-            parent_dir,
-            config["local"]["data_path"],
+            datas["parent_dir"],
+            datas["config"]["local"]["data_path"],
             "nobel_laureates_data",
         )
         if not os.path.exists(self.local_path):
             os.makedirs(self.local_path)
-        self.partition_path = config["nobelprize_laureates_api"]["nobelprize_laureate_path"]
-        self.local = NobelLaureateLocalUpload(logger_obj)
-        self.api = NobelprizeLaureatesFromApi(logger_obj)
-        self.s3_client = S3Details(logger_obj)
+        self.partition_path = datas["config"]["nobelprize_laureates_api"][
+            "nobelprize_laureate_path"
+        ]
+        self.local = NobelLaureateLocalUpload(self.logger)
+        self.api = NobelprizeLaureatesFromApi(self.logger, datas["config"])
+        self.s3_client = S3Details(self.logger)
         self.logger.info("created the instance for the class")
 
-    def fetch_nobelprize_laureate_from_api_each_year(
-        self,
-    ):
+    def fetch_nobelprize_laureate_from_api_each_year(self):
         """This method fetches the nobel prize and laureates details from the api
         for each year between the given years"""
         years_list = []
@@ -69,7 +65,7 @@ class NobelPrizeLaureatesPartitionS3:
                 years_list.append(self.startyear)
             self.logger.info(f"Created a list for the given years{years_list}")
         except Exception as err:
-            self.logger.error(f"Cannot get the resposnse from the given list of years{err}")
+            self.logger.error(f"Cannot get the list of years {err}")
             print(err)
             years_list = None
         return years_list
@@ -86,10 +82,7 @@ class NobelPrizeLaureatesPartitionS3:
                 response = self.api.pull_nobelprizes_laureates_from_api(self.endpoint, year)
                 if response is not None:
                     self.logger.info(f"Got the response from api for the year {year}")
-                    if self.endpoint == "nobelPrizes":
-                        df_data = pd.DataFrame(response["nobelPrizes"])
-                    else:
-                        df_data = pd.DataFrame(response["laureates"])
+                    df_data = pd.DataFrame(response[self.endpoint])
                     if not df_data.empty:
                         self.create_json_file_partition(df_data, year)
                 else:
@@ -103,11 +96,7 @@ class NobelPrizeLaureatesPartitionS3:
             )
         return df_data
 
-    def create_json_file_partition(
-        self,
-        df_data,
-        award_year,
-    ):
+    def create_json_file_partition(self, df_data, award_year):
         """This method creates a temporary json file,create partition path and upload to s3"""
         try:
             file_name = f"{self.endpoint}_{award_year}.json"
@@ -116,12 +105,16 @@ class NobelPrizeLaureatesPartitionS3:
                 orient="records",
                 lines=True,
             )
-            self.logger.info(
-                f"Created the json file for {self.endpoint} response for {award_year} "
-            )
+            self.logger.info(f"Created the json file for {award_year} ")
             partition_path = self.put_partition_path(award_year)
             copy_source = self.local_path + "/" + file_name
-            file_name=self.upload_to_s3(copy_source,partition_path, file_name)
+            file_name = self.local.upload_parition_s3_local(
+                self.local_path,
+                copy_source,
+                file_name,
+                partition_path,
+            )
+            # file_name=self.upload_to_s3(copy_source,partition_path, file_name)
         except Exception as err:
             self.logger.error(f"Cannot create a json file for{file_name}")
             print("Canot create a json file", err)
@@ -135,7 +128,7 @@ class NobelPrizeLaureatesPartitionS3:
             partition_path = f"{self.partition_path}/{self.endpoint}/pt_year={award_year}"
             print(partition_path)
             self.logger.info(
-                "Created the partition path for the given year{self.endpoint} and {year}"
+                f"Created the partition path for the given {self.endpoint} and {award_year}"
             )
         except Exception as err:
             self.logger.error(f"Cannot made a partition becausee of {err}")
@@ -162,10 +155,12 @@ def valid_year(year):
     """This method checks for the valid endpoint"""
     try:
         if year.isdigit() and len(year) == 4:
-            year_valid = year
+            year_valid = int(year)
+            datas["logger"].info(f"The given year is a valid year {year}")
         else:
             raise ValueError
     except ValueError:
+        datas["logger"].info(f"{year} is not a valid year.It must be in format YYYY")
         msg = f" {year} is not a valid year.It must be in format YYYY"
         year_valid = year
         raise argparse.ArgumentTypeError(msg)
@@ -174,25 +169,6 @@ def valid_year(year):
 
 def main():
     """This is the main method for this module"""
-    log_dir = os.path.join(
-        parent_dir,
-        config["local"]["log_path"],
-        "nobel_laureate_api_log",
-    )
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-    log_file = os.path.join(log_dir, "nobel_laureate_logfile.log")
-    logging.basicConfig(
-        level=logging.INFO,
-        filename=log_file,
-        datefmt="%d-%b-%y %H:%M:%S",
-        format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
-        filemode="a",
-    )
-    logger_obj = logging.getLogger("Rotating Log")
-    handler = RotatingFileHandler(log_file, maxBytes=10, mode="a")
-    #   ,backupCount=3)
-    logger_obj.addHandler(handler)
     parser = argparse.ArgumentParser(
         description="This argparser is to get the required date_of_joining need to filter"
     )
@@ -202,12 +178,7 @@ def main():
         type=valid_year,
         default=int(date.today().year) - 1,
     )
-    parser.add_argument(
-        "--toyear",
-        help="Enter the end year YYYY",
-        type=valid_year,
-    )
-    logger_obj.info("Checked the given years and are valid")
+    parser.add_argument("--toyear", help="Enter the end year YYYY", type=valid_year)
     parser.add_argument(
         "endpoint",
         choices=["nobelPrizes", "laureates"],
@@ -215,7 +186,6 @@ def main():
     )
     args = parser.parse_args()
     api_details = NobelPrizeLaureatesPartitionS3(
-        logger_obj,
         args.fromyear,
         args.toyear,
         args.endpoint,
