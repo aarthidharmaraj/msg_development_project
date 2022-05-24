ff8e687c084ff0d25a91e84e2dc3c2979018ebda
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-24 18:25:22 +0530

    Module for s3 credentials

--- load_hostaspire_file_sftp_to_s3/aws_s3/s3_details.py
+++ load_hostaspire_file_sftp_to_s3/aws_s3/s3_details.py
@@ -1,13 +1,14 @@
 """This module has S3 folder details and actions on that folder"""
-import configparser
 import boto3
 from botocore.exceptions import ClientError
+
 # from fixtures import s3_mock
+
 
 class S3Details:
     """This class has the methods for s3 service"""
 
-    def __init__(self, logger,config):
+    def __init__(self, logger, config):
         """This is the init method of the class S3Service"""
         self.client = boto3.client(
             "s3",
@@ -16,17 +17,13 @@ class S3Details:
         )
         # self.client = s3_mock
         self.logger = logger
-        self.bucket_name = config["s3"]["bucket"]
-        self.bucket_path = config["s3"]["bucket_path"]
 
-    def upload_file(self, file, path):
+    def upload_file(self, file, bucket_path, key):
         """This method gets file from sftp,uploads into s3 bucket in the given path"""
         try:
-            file = self.client.put_object(
-                Bucket=self.bucket_name, Body=file, Key=self.bucket_path + path
-            )
+            file = self.client.put_object(Bucket=self.bucket_name, Body=file, Key=bucket_path + key)
             # self.client.upload_file(file,
-            #     self.bucket_name,self.bucket_path+path
+            #     self.bucket_name,bucket_path+key
             # )
             self.logger.info("The file has been uploaded to s3")
         except Exception as err:
@@ -35,17 +32,15 @@ class S3Details:
             file = None
         return file
 
-    def get_list_of_files_in_s3(self, prefix):
+    def get_list_of_files_in_s3(self, bucket_path):
         """This method get the list of files from the given bucket and path in s3"""
         file_list = []
         try:
-            objects = self.client.list_objects_v2(
-                Bucket=self.bucket_name, prefix=self.bucket_path + prefix
-            )
+            objects = self.client.list_objects_v2(Bucket=self.bucket_name, prefix=bucket_path)
             for obj in objects["Contents"]:
                 if obj["Key"].endswith(".zip"):
                     file_list.append(obj["Key"].split("/"[-1]))
         except ClientError as err:
-            self.logger.error("Cannot get the list of files from s3 in the given path %s",err)
+            self.logger.error("Cannot get the list of files from s3 in the given path %s", err)
             file_list = None
         return file_list
