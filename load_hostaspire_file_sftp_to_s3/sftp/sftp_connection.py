d8199b3abe7e6ffd5218ca764ff136e0c9902fee
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-24 18:25:56 +0530

    Module for sftp conncetion and list files

--- load_hostaspire_file_sftp_to_s3/sftp/sftp_connection.py
+++ load_hostaspire_file_sftp_to_s3/sftp/sftp_connection.py
@@ -1,6 +1,5 @@
 """This module has script for the performance on sftp folder"""
 import pysftp
-
 # from fixtures import mock_sftp
 
 class SftpConnection:
@@ -9,20 +8,31 @@ class SftpConnection:
     def __init__(self,logger,config):
         """This is the init method of the class of SftpCon"""
         self.config=config
-        self.conn = pysftp.Connection(
-            host=config["SFTP"]["host"],
-            username=config["SFTP"]["username"],
-            password=config["SFTP"]["password"],
-        )
-        # self.conn=mock_sftp
         self.logger = logger
-        self.sftp_path = config["SFTP"]["sftp_path"]
 
-    def list_files(self):
+    def sftp_connection(self):
+        """This method connects with sftp server with the given details"""
+        
+        try:
+            conn = pysftp.Connection(
+            host=self.config["SFTP"]["host"],
+            username=self.config["SFTP"]["username"],
+            password=self.config["SFTP"]["password"],
+        )
+            self.logger.info("Successfully connected to sftp server with the given credentials")
+        except Exception as err:
+            self.logger.info("Cannot able to connect to sftp server with the given credentials")
+            conn=None
+        return conn
+    
+    def list_files(self,sftp_path):
         """This method gets the list of files names for the given sftp path by filtering"""
         try:
+            conn=self.sftp_connection()
+            # conn=mock_sftp
+            
             sftp_file_list = [
-                file for file in self.conn.listdir(self.sftp_path)
+                file for file in conn.listdir(sftp_path)
             ]
             self.logger.info("Received the files from sftp server")
         except Exception as err:
