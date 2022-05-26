2b1222c0c7a91c88f98b3f2aaddf229e41620526
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-05-26 17:56:37 +0530

    Module to list files, get latest files, and upload methods in local

--- load_hostaspire_file_sftp_to_s3/sftp_to_s3_upload_local.py
+++ load_hostaspire_file_sftp_to_s3/sftp_to_s3_upload_local.py
@@ -6,13 +6,13 @@ import shutil
 class MoveSftpToS3Local:
     """This class has the methods that do in local folder for sftp and s3"""
 
-    def __init__(self, logger, config):
+    def __init__(self, logger):
         self.logger = logger
 
-    def list_files_sftp_local(self,sftp_path):
+    def list_files_sftp_local(self, sftp_path):
         """This method gets the list of files names for the given local sftp path by filtering"""
         try:
-            sftp_file_list =list(os.listdir(sftp_path))
+            sftp_file_list = list(os.listdir(sftp_path))
             self.logger.info("Received the list of files from sftp local")
         except Exception as err:
             sftp_file_list = None
@@ -20,18 +20,18 @@ class MoveSftpToS3Local:
             print("Cannot get the files from local sftp", err)
         return sftp_file_list
 
-    def get_latest_files_from_local(self, local_path, source_path,sftp_path):
+    def get_latest_files_from_local(self, local_path, source_path, sftp_path):
         """This method gets the latest files from local sftp compared with local s3"""
         try:
             temp_s3_files = []
             s3_file_path = local_path + "/" + source_path
-            for (root_path,dirs,files) in os.walk(s3_file_path):
+            for (root_path, dirs, files) in os.walk(s3_file_path):
                 for file in files:
                     temp_s3_files.append(file)
             self.logger.info("Got the files %s from source path of local s3", temp_s3_files)
             temp_sftp_files = self.list_files_sftp_local(sftp_path)
             self.logger.info("Got the files %s from local sftp", temp_sftp_files)
-            latest_files = list(set(temp_s3_files).symmetric_difference(set(temp_sftp_files)))
+            latest_files = list(set(temp_sftp_files).difference(set(temp_s3_files)))
             print("Newly uploaded files", latest_files)
             self.logger.info("Got the newly uploaded files %s from local s3", latest_files)
         except Exception as err:
@@ -39,8 +39,32 @@ class MoveSftpToS3Local:
             latest_files = None
         return latest_files
 
+    def upload_zip_text_file_to_given_path(
+        self, local_path, partition_path, sources, file_name, temp_s3_path
+    ):
+        """This method takes the zip source and filename and upload the zip to
+        source path, extracted text file to stage path."""
+        try:
+            self.logger.info(
+                "Uploading the file %s from sftp server to the given path %s",
+                file_name,
+                temp_s3_path,
+            )
+            file = self.upload_partition_s3_local(
+                local_path + "/" + temp_s3_path,
+                sources,
+                file_name,
+                partition_path,
+            )
+        except Exception as err:
+            self.logger.error(
+                "Cannot upload the file %s to the given path %s , %s", file_name, temp_s3_path, err
+            )
+            file = None
+        return file
+
     def upload_partition_s3_local(self, local_path, copy_source, file_name, partition_path):
-        """This method uploads weatherdata in the partition path in the form of json"""
+        """This method uploads given file in the partition path in local s3"""
         try:
             new_dir = local_path + "/" + partition_path + "/"
             if not os.path.exists(new_dir):
@@ -51,7 +75,6 @@ class MoveSftpToS3Local:
             self.logger.info(
                 "Successfully uploaded file %s in the given path %s", file_name, partition_path
             )
-
         except Exception as err:
             print("Cannot upload the  file in the given path:", err)
             self.logger.error(f"Cannot upload the file in the given path{err}")
