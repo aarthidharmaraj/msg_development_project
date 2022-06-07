6744d5c3ed9bc3cf1b2f7e383856cef01c31121a
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-06-07 18:31:17 +0530

    Module to upload the json file in local s3 path

---
+++ fetch_data_freesound_api_authentication/fetch_data_partition_upload_local.py
@@ -0,0 +1,28 @@
+"""This module has methods to upload the files in the local path with the partition path"""
+import os
+import shutil
+
+
+class ApiDataPartitionUploadLocal:
+    """This class has the methods that do in local folder for sftp and s3"""
+
+    def __init__(self, logger):
+        self.logger = logger
+
+    def upload_partition_s3_local(self, local_path, copy_source, file_name, partition_path):
+        """This method uploads given file in the partition path in local s3"""
+        try:
+            new_dir = local_path + "/" + partition_path + "/"
+            if not os.path.exists(new_dir):
+                os.makedirs(new_dir)
+            shutil.copy(copy_source, new_dir)
+            os.remove(copy_source)
+            print("Successfully uploaded file in the given path",partition_path)
+            self.logger.info(
+                "Successfully uploaded file %s in the given path %s", file_name, partition_path
+            )
+        except Exception as err:
+            print("Cannot upload the  file in the given path:", err)
+            self.logger.error(f"Cannot upload the file in the given path{err}")
+            file_name = None
+        return file_name
