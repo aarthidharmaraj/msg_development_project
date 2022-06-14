461e888cf30395ed3705ba3be9b8144f4004c963
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-06-14 18:24:05 +0530

    Module having methods to get data from api based on order id, partition them and upload to s3

---
+++ metamuseum_api/fetch_data_from_api_upload_s3.py
@@ -0,0 +1,170 @@
+"""This method gets the meta museum object data from api based on the given object id
+which are available in api ,partition them based on object id and upload to s3"""
+import os
+import argparse
+from datetime import date
+import sys
+import pandas as pd
+from logger_path.logger_object_path import LoggerPath
+from aws_s3.s3_details import S3Details
+from fetch_data_partition_upload_local import ApiDataPartitionUploadLocal
+from get_response_metamuseum_api import MetaMuseumApi
+
+datas = LoggerPath.logger_object("metamuseum_api")
+api = MetaMuseumApi(datas["config"]["metamuseum_api"], datas["logger"])
+
+
+class MetaMuseumObjectRecordS3:
+    """This method fetches the record of an object from meta museum api based on
+    the given object id,partition them and upload to s3"""
+
+    def __init__(self):
+        """This is the init method for the class PublicHolidaysLongWeekendS3"""
+        self.section = datas["config"]["metamuseum_api"]
+        self.logger = datas["logger"]
+        self.s3_client = S3Details(self.logger, datas["config"])
+        self.local = ApiDataPartitionUploadLocal(self.logger)
+        self.api = MetaMuseumApi(self.section, self.logger)
+        self.logger.info("Successfully created the instance of the class")
+
+    def get_response_from_api(self,obj_id):
+        """This method fetches the record of object from meta museum api based on the object id
+        parameters :  obj_id - the id of object for which datas are needed"""
+        try:
+                self.logger.info("Getting response from api for %s id", obj_id)
+                response = self.api.get_endpoint_for_object_record(obj_id
+                )
+                # print(response)
+                file_name = f"record_for_{obj_id}.json"
+                self.get_dataframe_for_response(response,obj_id, file_name)
+        except Exception as err:
+            self.logger.error(
+                "Cannot able to get response from api for the obj_id %s - %s", obj_id, err
+            )
+            response = None
+            print(err)
+            sys.exit("System has terminated for fail in getting response from api")
+        return response
+
+    def get_dataframe_for_response(self, response,obj_id, file_name):
+        """This method gets response from api,convert to dataframe and create json file for that
+        parameters : response - response from api
+                    obj_id - the id for which object records are fetched
+                    file_name - file_name to be uploaded in s3"""
+        try:
+            if response is not None:
+                self.logger.info("Got the response from api for %s",obj_id)
+                df_data=pd.DataFrame.from_dict(response, orient='index')
+                df_data = df_data.transpose()
+                if not df_data.empty:
+                    partition_path = self.put_partition_path(obj_id)
+                    self.create_json_file_partition(
+                        df_data,file_name, partition_path,obj_id
+                    )
+            else:
+                self.logger.info("No responses from api for the %s", obj_id)
+                df_data = None
+        except Exception as err:
+            self.logger.error("Cannot create the dataframe for the given response %s", err)
+            df_data = None
+        return df_data
+
+    def create_json_file_partition(self, df_data,file_name, partition_path,obj_id):
+        """This method creates a temporary json file,create partition path and upload to s3
+        parameters : df_data - dataframe created from the response
+                    file_name - file_name to be uploaded in s3
+                    partition_path - partition based on obj_id"""
+        try:
+            self.logger.info("Got the dataframe for the object id %s",obj_id)
+            s3_path = self.section["s3_path"]
+            local_path = LoggerPath.local_download_path("metamuseum_api_datas")
+            df_data.to_json(
+                local_path + "/" + file_name,
+                orient="records",
+                lines=True,
+            )
+            self.logger.info("Created the json file for %s", obj_id)
+            copy_source = local_path + "/" + file_name
+            file_name = self.local.upload_partition_s3_local(
+                local_path,
+                copy_source,
+                file_name,
+                s3_path  + "/" + partition_path,
+            )
+            # file=self.upload_to_s3(s3_path,copy_source,s3_path  + "/" + partition_path, file_name)
+        except Exception as err:
+            self.logger.error("Cannot create a json file for file_name %s", file_name)
+            print("Canot create a json file", err)
+            file_name = None
+        return file_name
+
+    def put_partition_path(self, obj_id):
+        """This method will make partion path based on obj_id
+        and avoid overwrite of file and upload to local"""
+        try:
+            partition_path = f"pt_id={obj_id}"
+            self.logger.info("Created the partition path for the given %s", obj_id)
+        except Exception as err:
+            self.logger.error("Cannot made a partition for %s because of %s", obj_id,err)
+            partition_path = None
+        return partition_path
+
+    def upload_file_to_s3(self, bucket_path, source, partition_path, file_name):
+        """This method used to upload the file to s3 in the partiton created
+        parameters: file_name - name of the file to be uploaded
+                    source - the source which has the file and its data
+                    partition_path - path has partition based on endpoint and date"""
+        try:
+            key = partition_path + "/" + file_name
+            self.logger.info(
+                "The file %s being uploaded to s3 in the given path %s", file_name, key
+            )
+            file = self.s3_client.upload_file(source, self.section["bucket"], bucket_path, key)
+            print("the file has been uploaded to s3 in the given path")
+            os.remove(source)
+        except Exception as err:
+            self.logger.error("Cannot upload the files to s3 in the given path %s", err)
+            file = None
+        return file
+
+
+# def valid_object_id(obj_id):
+#     """This method checks the available object id in metamuseum api"""
+#     try:
+#         endpoint = "objects"
+#         response = api.get_response_from_api(endpoint)
+#         # print(response)
+#         # available_reg = [reg["objectIDs"] for reg in response if response is not None]
+#         print(type(response["objectIDs"]))
+#         if obj_id in response["objectIDs"]:
+#             valid_id = obj_id
+#             datas["logger"].info("The given id %s is available in meta museum api",obj_id)
+#         else:
+#             print("No")
+#             raise Exception
+#         # valid_id=None
+#     except Exception as err:
+#         datas["logger"].error(
+#             "The given id %s is not available in meta museum api %s", obj_id, err
+#         )
+#         print(err)
+#         msg = f" {obj_id} is not an available object in meta museum api."
+#         valid_id = None
+#         raise argparse.ArgumentTypeError(msg)
+#     return valid_id
+
+
+def main():
+    """This is the main method for this module"""
+    parser = argparse.ArgumentParser(
+        description="This argparser is to get the object id to fetch objects data from meta museum api"
+    )
+    parser.add_argument(
+        "--objectid", help="Enter the object id", type=int, required=True
+    )
+    args = parser.parse_args()
+    api_details = MetaMuseumObjectRecordS3()
+    api_details.get_response_from_api(args.objectid)
+    
+if __name__ == "__main__":
+    main()
