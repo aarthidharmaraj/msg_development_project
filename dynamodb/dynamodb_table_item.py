47066f8720dc8eba3cce1f6681c7923db1575faa
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-07-06 18:17:14 +0530

    Module to create table, insert item, get item and update item from the table in dynamodb

--- dynamodb/dynamodb_table_item.py
+++ dynamodb/dynamodb_table_item.py
@@ -8,40 +8,40 @@ class DynamoDB:
         self.logger=logger
         self.resource= boto3.resource('dynamodb')
     
-    def create_table_dynamodb(self,table_name):
-        """This method will create a table with the given table name in dynamoDB"""
+    def create_table_dynamodb(self,**kwargs):
+        """This method will create a table with the arguments in dynamoDB"""
         try:
-            table = self.resource.create_table()
+            table = self.resource.create_table(**kwargs)
         except Exception as err:
             self.logger.error("Cannot create the table in dynamoDB %s",err)
             table=None
         return table
     
-    def put_item_table(self,table_name):
+    def put_item_table(self,table_name,**kwargs):
         """This method put an item in a table"""
         try:
             table = self.resource.Table(table_name)
-            item = table.put_item()
+            item = table.put_item(**kwargs)
         except Exception as err:
             self.logger.error("Cannot put an item in the table %s",err)
             item=None
         return item
     
-    def get_item_from_table(self,table_name):
+    def get_item_from_table(self,table_name,**kwargs):
         """This method gets an item from the table"""
         try:
             table = self.resource.Table(table_name)
-            item = table.get_item()
+            item = table.get_item(**kwargs)
         except Exception as err:
             self.logger.error("Cannot get an item from the table %s",err)
             item=None
         return item
         
-    def update_item_from_table(self,table_name):
+    def update_item_from_table(self,table_name,**kwargs):
         """This method updates an item in the table"""
         try:
-            table = self.resource.Table(table_name)
-            item= table.update_item()
+            table = self.resource.Table(table_name,**kwargs)
+            item= table.update_item(**kwargs)
         except Exception as err:
             self.logger.error("Cannot update item in the table %s",err)
             item=None
